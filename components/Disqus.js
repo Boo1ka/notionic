207bd05d0dfcc4da306d2ae5d10ce1bb9f232327
Boo1ka <egor.boo1ka@gmail.com>
2023-01-21 17:51:26 +0500

    dewd

--- components/Disqus.js
+++ components/Disqus.js
@@ -1,4 +1,4 @@
-import { DiscussionEmbed } from 'disqus-react';
+import { DiscussionEmbed } from 'disqus-react'
 
 <DiscussionEmbed
   shortname='um-dom-dev'
@@ -7,7 +7,7 @@ import { DiscussionEmbed } from 'disqus-react';
       url: 'umdom-git-dev-boo1ka.vercel.app/' + this.props.post.slug,
       identifier: this.props.post.id,
       title: this.props.post.title,
-      language: 'ru-RU' //e.g. for Traditional Chinese (Taiwan)	
+      language: 'ru-RU' // e.g. for Traditional Chinese (Taiwan)
     }
   }
 />
