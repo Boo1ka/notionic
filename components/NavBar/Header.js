2a287d5ab57ad53a8715290d3b705e742685c5aa
Boo1ka <egor.boo1ka@gmail.com>
2023-01-26 21:26:57 +0500

    убираем иероглиф

--- components/NavBar/Header.js
+++ components/NavBar/Header.js
@@ -172,7 +172,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
         ref={navRef}
       >
         <div className='flex items-center'>
-          <Link passHref href='/' scroll={false}>
+          {/* <Link passHref href='/' scroll={false}>
             <a aria-label={BLOG.title}>
               <motion.div className='h-6 hover:text-blue-500 dark:hover:text-blue-500 fill-current'>
                 <svg
@@ -188,7 +188,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
                 </svg>
               </motion.div>
             </a>
-          </Link>
+          </Link> */}
           {navBarTitle ? (
             <p
               className={`ml-2 font-medium ${!showTitle ? 'hidden' : 'hidden xl:block'
